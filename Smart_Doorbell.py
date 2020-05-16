090a013ed7d4ec1758698eeb03e99ea37812991d
Josh Stockwell <Josh.Stockwell@gmail.com>
2020-05-16 10:06:36 +0100

    Init project files

---
+++ Smart_Doorbell.py
@@ -0,0 +1,93 @@
+import numpy as np
+import cv2
+import imutils
+import time
+import os
+
+picFolder = '/home/pi/Documents/PyFiles/SmartDoorbell/Pics/'
+vidFolder = '/home/pi/Documents/PyFiles/SmartDoorbell/Videos/'
+
+def mask_img(img):
+
+    mask = np.zeros((img.shape[0], img.shape[1]), dtype="uint8")
+
+    pts = np.array([[151, 272], [346, 268], [355, 353], [246, 354]])
+    cv2.fillConvexPoly(mask, pts,255)
+    
+    pts = np.array([[533, 350],[950, 364],[1021, 441],[755, 589]])
+    cv2.fillConvexPoly(mask, pts,255)
+
+    masked = cv2.bitwise_and(img, img, mask=mask)
+    
+    grey = imutils.resize(masked, width = 200)
+
+    grey = cv2.cvtColor(grey, cv2.COLOR_BGR2GRAY)
+
+    grey = cv2.GaussianBlur(grey, (11, 11), 0)
+
+    return masked, grey
+
+# Counter Variable for analysis
+counter = 0
+
+while True:
+    counter = counter + 1
+    print ("  ")
+    print ("---- Times through loop since starting:" + str(counter) + " ----")
+    print ("  ")
+
+    # take 1st and 2nd image to compare
+
+    command = 'raspistill -w 1280 -h 720 -vf -hf -t 1000 -tl 1000 -o ' + picFolder + 'test%0d.jpg'
+
+    os.system(command)
+
+    print ("Captured First and Second Img for Analysis")
+    
+    # mask images
+    test1 = cv2.imread(picFolder + "test0.jpg")
+    test2 = cv2.imread(picFolder + "test1.jpg")
+
+    masked1, grey1 = mask_img(test1)
+    masked2, grey2 = mask_img(test2)
+
+    # Compare the two images
+    pixel_thres = 50
+
+    detector_total = np.uint64(0)
+    detector = np.zeros((grey2.shape[0], grey2.shape[1]), dtype="uint8")
+    
+    # Pixel by Pixel comp
+    for i in range(0, grey2.shape[0]):
+        for j in range(0, grey2.shape[1]):
+            if abs(int(grey2[i, j]) - int(grey1[i, j])) > pixel_thres:
+                detector[i,j] = 255
+
+    # sum the detector array
+    detector_total = np.uint64(np.sum(detector))
+    print ("detector total =" , detector_total)
+
+    if detector_total > 30000:
+        print ("SmartDoorbell has detected something!")
+         
+        
+        # define a unique name for the videofile
+        timestr = time.strftime("doorbell-%Y%m%d-%H%M%S")
+
+        command2 = 'raspivid -t 10000 -w 1280 -h 720 -vf -hf -fps 30 -o ' + vidFolder + timestr + '.h264'
+        os.system(command2)
+
+        print("Finished recording, converting to mp4...")
+
+        command3 = 'MP4Box -fps 30 -add ' + timestr + '.h264 ' + timestr + '.mp4'
+        # os.system(command3)
+        
+        print("Finished converting file, available for review")
+
+        #write masked images to file
+        cv2.imwrite(picFolder + "grey1.jpg", grey1)
+        cv2.imwrite(picFolder + "grey2.jpg", grey2)
+        cv2.imwrite(picFolder + "masked1.jpg", masked1)
+        cv2.imwrite(picFolder + "masked2.jpg", masked2)
+    else: 
+        print("Nothing detected yet...")
---
+++ doorbell_mask.py
@@ -0,0 +1,70 @@
+import numpy as np
+import cv2
+import imutils
+import time
+import os
+
+class SelectROI(object):
+    def __init__(self, name, im):
+        self.image = im
+        self.winname = name
+
+        cv2.namedWindow(name)
+        self.coords = []
+        self.dragging = False
+        self._update()
+
+    def _mouse_cb(self, event, x, y, flags, parameters):
+        # Record starting (x,y) coordinates on left mouse button click
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.coords[:] = [(x, y)]
+            self.dragging = True
+
+        elif event == 0 and self.dragging:
+            self.coords[1:] = [(x, y)]
+
+        # Record ending (x,y) coordintes on left mouse bottom release
+        elif event == cv2.EVENT_LBUTTONUP:
+            self.coords[1:] = [(x, y)]
+            self.dragging = False
+            xs, ys = list(zip(*self.coords))
+            self.coords = [(min(xs), min(ys)),
+                           (max(xs), max(ys))]
+            print('roi:', self.coords)
+
+        # Clear drawing boxes on right mouse button click
+        elif event == cv2.EVENT_RBUTTONDOWN:
+            self.coords = []
+            self.dragging = False
+
+        self._update()
+
+    def _update(self):
+        im = self.image.copy()
+        if len(self.coords) == 2:
+            cv2.rectangle(im, self.coords[0], self.coords[1], (0, 255, 0), 2)
+        cv2.imshow(self.winname, im)
+
+    def __call__(self):
+        cv2.setMouseCallback(self.winname, self._mouse_cb)
+        cv2.waitKey()
+        cv2.destroyWindow(self.winname)
+        return self.coords if self.coords else None
+
+def select_ROI(name, im):
+    s=SelectROI(name, im)
+    return s()
+
+
+picFolder = 'home/pi/Documents/SmartDoorbell/Pics/'
+
+def mask_img(img):
+
+# This enables point selection printing coordinates to terminal
+    for i in range(0,4):
+        bbox = select_ROI("Original", img)
+        print(bbox)
+    
+# works if image is in the same folder as the py file
+imgtomask = cv2.imread(folder + 'camera_location.jpg')
+test1 = mask_img(imgtomask)
