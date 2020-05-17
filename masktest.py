35fc48fdde8b3de4c9e64f8db37a0074f7f4944a
Josh Stockwell <Josh.Stockwell@gmail.com>
2020-05-17 11:54:42 +0100

    show on screen

--- masktest.py
+++ masktest.py
@@ -6,12 +6,15 @@ import imutils
 def mask_img(img):
     # set up a blank mask array of zeros
     mask = np.zeros((img.shape[0], img.shape[1]), dtype="uint8")
+
     # load the first polygon
     pts = np.array([[449, 315], [542, 318], [538, 365], [460, 358]])
     cv2.fillConvexPoly(mask, pts, 255)
+
     # load the second polygon
     pts = np.array([[748, 426], [883, 432], [1018, 504], [
                    916, 539], [898, 647], [827, 621], [838, 517]])
+
     cv2.fillConvexPoly(mask, pts, 255)
 
     masked = cv2.bitwise_and(img, img, mask=mask)
@@ -26,5 +29,7 @@ def mask_img(img):
 
 
 picFolder = r'/home/pi/SmartDoorbell/Pics/camera_location.jpg'
-file = cv2.imshow('image', picFolder)
-mask_img(file)
+image = cv2.imread(picFolder)
+cv2.imshow('image', image)
+cv2.waitKey()
+#mask_img(file)
