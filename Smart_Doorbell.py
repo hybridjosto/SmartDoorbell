3ce281ce464e8d5f1dbfd8fa0a0d2c312067d492
Josh <josh.stockwell@gmail.com>
2020-05-17 10:35:48 +0100

    syntax updates

--- Smart_Doorbell.py
+++ Smart_Doorbell.py
@@ -7,25 +7,28 @@ import os
 picFolder = '/home/pi/SmartDoorbell/Pics/'
 vidFolder = '/home/pi/SmartDoorbell/Video/'
 
+
 def mask_img(img):
 
     mask = np.zeros((img.shape[0], img.shape[1]), dtype="uint8")
 
     pts = np.array([[449, 315], [542, 318], [538, 365], [460, 358]])
-    cv2.fillConvexPoly(mask, pts,255)
-    
-    pts = np.array([[748, 426],[883, 432],[1018, 504],[916, 539],[898, 647],[827, 621],[838, 517]])
-    cv2.fillConvexPoly(mask, pts,255)
+    cv2.fillConvexPoly(mask, pts, 255)
+
+    pts = np.array([[748, 426], [883, 432], [1018, 504], [
+                   916, 539], [898, 647], [827, 621], [838, 517]])
+    cv2.fillConvexPoly(mask, pts, 255)
 
     masked = cv2.bitwise_and(img, img, mask=mask)
-    
-    grey = imutils.resize(masked, width = 200)
+
+    grey = imutils.resize(masked, width=200)
 
     grey = cv2.cvtColor(grey, cv2.COLOR_BGR2GRAY)
 
     grey = cv2.GaussianBlur(grey, (11, 11), 0)
 
     return masked, grey
+
 
 # Counter Variable for analysis
 counter = 0
@@ -43,7 +46,7 @@ while True:
     os.system(command)
 
     print ("Captured First and Second Img for Analysis")
-    
+
     # mask images
     test1 = cv2.imread(picFolder + "test0.jpg")
     test2 = cv2.imread(picFolder + "test1.jpg")
@@ -56,21 +59,20 @@ while True:
 
     detector_total = np.uint64(0)
     detector = np.zeros((grey2.shape[0], grey2.shape[1]), dtype="uint8")
-    
+
     # Pixel by Pixel comp
     for i in range(0, grey2.shape[0]):
         for j in range(0, grey2.shape[1]):
             if abs(int(grey2[i, j]) - int(grey1[i, j])) > pixel_thres:
-                detector[i,j] = 255
+                detector[i, j] = 255
 
     # sum the detector array
     detector_total = np.uint64(np.sum(detector))
-    print ("detector total =" , detector_total)
+    print ("detector total =", detector_total)
 
     if detector_total > 30000:
         print ("SmartDoorbell has detected something!")
-         
-        
+
         # define a unique name for the videofile
         timestr = time.strftime("doorbell-%Y%m%d-%H%M%S")
 
@@ -81,13 +83,13 @@ while True:
 
         command3 = 'MP4Box -fps 30 -add ' + timestr + '.h264 ' + timestr + '.mp4'
         # os.system(command3)
-        
+
         print("Finished converting file, available for review")
 
-        #write masked images to file
+        # write masked images to file
         cv2.imwrite(picFolder + "grey1.jpg", grey1)
         cv2.imwrite(picFolder + "grey2.jpg", grey2)
         cv2.imwrite(picFolder + "masked1.jpg", masked1)
         cv2.imwrite(picFolder + "masked2.jpg", masked2)
-    else: 
+    else:
         print("Nothing detected yet...")
